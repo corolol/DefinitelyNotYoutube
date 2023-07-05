45a1a721c15d7dd09e23ea54b42ed7f384779023
corolol <luk.jon@tlen.pl>
2023-07-05 18:17:28 +0200

    Updated account page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,24 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Video;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
-    public function index(Request $req, AuthorizationCheckerInterface $auth): Response
+    public function index(Request $req, EntityManagerInterface $em, UserInterface $user): Response
     {
-        $user_param = $req->query->get('u');
-        $is_auth = $auth->isGranted('IS_AUTHENTICATED');
-        echo $is_auth;
-        
+        $latestVideo = $em->getRepository(Video::class)->findBy(['author' => $user], ['upload_date' => 'DESC'], 1);
+        if (sizeof($latestVideo) > 0) $latestVideo = $latestVideo[0];
+        else $latestVideo = null;
         
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+            'latest_video' => $latestVideo
         ]);
     }
 }
