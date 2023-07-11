6a2d007d91409c6cacba10a09bfbb794ac7c0e41
corolol <luk.jon@tlen.pl>
2023-07-11 13:09:12 +0200

    Added username regex contraint

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,18 +7,27 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new Regex(
+                        pattern: '/^[a-zA-Z0-9]+$/',
+                        message: 'Username can only consist of letters and numbers'
+                    )
+                ]
+            ])
             ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
