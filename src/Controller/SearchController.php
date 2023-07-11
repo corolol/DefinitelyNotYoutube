447eddd8499389d9c0d3a3c8856b31a7bce99ef9
corolol <luk.jon@tlen.pl>
2023-07-11 13:08:42 +0200

    Added searching by author's name

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Video;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,7 +21,10 @@ class SearchController extends AbstractController
         if (!$search) return $this->redirectToRoute("app_index");
 
         $result = $em->getRepository(Video::class)->createQueryBuilder('v')
+            ->innerJoin(User::class, 'u', Join::WITH, 'v.author = u.id')
             ->where('v.title LIKE :query')
+            ->orWhere('u.username LIKE :query')
+            ->addOrderBy('v.upload_date', 'DESC')
             ->setParameter('query', '%'.trim($search).'%')
             ->getQuery()
             ->execute()    
